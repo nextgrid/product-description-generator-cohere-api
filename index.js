e875fc9afc8296638936b5710be1e3aeb981dc3f
Laszlo Gaal <laszlo.gaal@newnative.ai>
2022-08-09 12:11:01 +0200

    starter

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const description = require("./routes/description.js");
 const app = express();
 const port = 3000;
 
@@ -9,7 +8,6 @@ app.get("/", (req, res) => {
   res.send("Hello World! I am a product description generator app using Cohere AI.");
 });
 
-app.use("/description", description);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
